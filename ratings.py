95dc5840aabcc5fd1c31ad5dbeecb973daac593b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-10 17:57:52 -0700

    notes for what we need to do next

--- ratings.py
+++ ratings.py
@@ -6,6 +6,8 @@ import sys
 
 file_name = sys.argv[1]
 
+#need to move the restaurant_info dictionary OUT of the function so
+#we will be able to get it from the other functions(to add user rating)
 
 def get_ratings_data(file_name):
     """Return dictionary with restaurant and rating data"""
@@ -23,15 +25,24 @@ def get_ratings_data(file_name):
         #NOTE:  rating is a string, which is fine for this implementation
         #but if later we need to order by rating we'll have to change to int
 def get_user_rating():
+    """Gets user rating of a restaurant"""
+
+    #ask user for rating
     print "Please rate a restaurant!"
     new_restaurant = raw_input("Enter a restaurant name:")
+    
+    #check that the rating is an integer and if not, re-prompt
     while True:
         try: 
             new_rating = int(raw_input("Enter rating for restaurant:"))
             break
         except ValueError: 
             print "Oops! Please enter a number."
-    print new_rating
+
+    #return the restaurant and rating
+    return new_restaurant, new_rating
+
+#new function to add new_restaurant and new_rating to the dictionary
 
 def print_ratings(restaurant_info):
     """Print statement on restaurant rating."""
