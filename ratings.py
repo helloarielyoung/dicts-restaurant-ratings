1b3af99cd3a67fb6e54b38af34d67ec75ead8656
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-10 17:09:57 -0700

    successfully got data in a dictionary

--- ratings.py
+++ ratings.py
@@ -14,8 +14,16 @@ def get_ratings(file_name):
     #split the lines at : and put in dictionary
     restaurant_info = {}
 
-    for rating in ratings:
-        restaurant = ratings.split(":")
+    for line in ratings:
+        restaurant, rating = line.split(":")
+        restaurant_info[restaurant] = rating.rstrip()
+
+        #NOTE:  rating is a string, which is fine for this implementation
+        #but if later we need to order by rating we'll have to change to int
+
+    #for testing
+    print restaurant_info
 
     #put the results in a dictionary
     #print the result in nice sentences in alphabetical order (hint use .items and sorted())   
+get_ratings(file_name)
